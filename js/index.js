3aac544e54cf596be365df01deacc092714d2e83
Jesse Brizzi <jbrizzi@cs.stonybrook.edu>
2015-03-22 23:32:22 -0400

    Clean up the files and added slideshow

--- js/index.js
+++ js/index.js
@@ -58,3 +58,14 @@ switchAccordion = function(e) {
     accordionToggles[i].addEventListener('click', switchAccordion, false);
   }
 })();
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
